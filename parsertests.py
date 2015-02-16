39e5defbb12da62fc132e89437b1ce408b85ec6b
Erik Lovlie <eriklovlie@gmail.com>
2015-02-16 11:03:28 +0100

    Fix parsertests script exit code.

--- parsertests.py
+++ parsertests.py
@@ -46,16 +46,28 @@ def detected_antlr_warnings(testout):
   return "reportAmbiguity" in testout
 
 def main():
-  n_not_passing = 0
+  n_total = 0
+  n_pass = 0
+  n_fail = 0
   p = Pool(4)
   for result in p.imap_unordered(run_test, [f for f in glob.glob("parsertests/*") if os.path.isdir(f)]):
+    n_total += 1
     status = statuscolor[result.status] + result.status + bcolors.ENDC
     if result.status != 'PASS':
-      n_not_passing += 1
+      if result.status == 'FAIL':
+        n_fail += 1
       print "{}: {} - {}".format(status, result.testname, result.testout)
     else:
+      n_pass += 1
       print "{}: {}".format(status, result.testname)
-  print "Summary: {} tests did not pass cleanly".format(n_not_passing)
+
+  print "Summary:"
+  print "- PASS: {}".format(n_pass)
+  print "- FAIL: {}".format(n_fail)
+  print "- WARN: {}".format(n_total - n_fail - n_pass)
+  if n_fail == 0:
+      return 0
+  return 1
 
 if __name__ == "__main__":
   sys.exit(main())
